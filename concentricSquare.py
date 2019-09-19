32df8556099d85f72ffdbdd7cc003710ac5aa41d
Pawan kumar <pawankumarpawankumar619@gmail.com>
2019-09-19 13:58:28 +0530

    Added file to Repository

---
+++ concentricSquare.py
@@ -0,0 +1,35 @@
+n = 4
+t = 2 * n - 1
+i = 0
+row = 0
+
+while i < t:
+
+    ch = n
+    j = 0
+    while j < row:
+        print(ch, end=" ")
+        ch -= 1
+        j += 1
+
+    ch = n - row
+    k = 0
+    while k < t - 2 * row:
+        print(ch, end=" ")
+        k += 1
+
+    ch = n - row + 1
+    l = 0
+    while l < row:
+        print(ch, end=" ")
+        ch += 1
+        l += 1
+
+    if i < t // 2:
+        row += 1
+    else:
+        row -= 1
+
+    i += 1
+    print()
+
